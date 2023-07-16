a0bfd36222245e483d44a1a0bf026d054db31780
Arda66 <ardaege66@hotmail.com>
2023-07-16 22:04:36 +0300

    First push

---
+++ App.js
@@ -0,0 +1,17 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
