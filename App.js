b486183112fd823adf96f053a7a0dbff676e0cb0
Arda66 <ardaege66@hotmail.com>
2023-07-16 22:42:26 +0300

    Rearranged the location of the pages.

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-
+import {HomeScreen} from './src/screens/pages';
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{headerShown: false}}
+          name="Home"
+          component={HomeScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,25 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      {/* <View style={styles.titleWrapper}> */}
+      <Text>Welcome To the ARtisan</Text>
+      {/* </View> */}
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  titleWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
